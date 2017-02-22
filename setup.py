e9efc6f2a4b5da7976407a1c3fe3408121606d3b
박현재/NE <hyeonjae.park@nhnent.com>
2017-02-22 15:24:04 +0900

    fix setup.py

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ setup(
     packages=['sanic'],
     platforms='any',
     install_requires=[
-        'uvloop>=0.5.3,platform_system!="Windows"',
+        'uvloop>=0.5.3',
         'httptools>=0.0.9',
         'ujson>=1.35',
         'aiofiles>=0.3.0',
